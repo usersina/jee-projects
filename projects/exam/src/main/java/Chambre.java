fd8f054fb778bcbb6483f82a4f4bd832bc34cece
usersina <benna.anis2@gmail.com>
2024-03-23 13:41:28 +0100

    integrate lombok

--- projects/exam/src/main/java/Chambre.java
+++ projects/exam/src/main/java/Chambre.java
@@ -1,36 +1,10 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Chambre {
     private int id;
     private String nom;
     private String etat;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getEtat() {
-        return etat;
-    }
-
-    public void setEtat(String etat) {
-        this.etat = etat;
-    }
-
-    public Chambre(int id, String nom, String etat) {
-        this.id = id;
-        this.nom = nom;
-        this.etat = etat;
-    }
 }
