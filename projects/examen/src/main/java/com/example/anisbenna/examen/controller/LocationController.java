3646f7b3fdb117028a0d69faaf3357d860033865
usersina <benna.anis2@gmail.com>
2024-05-25 09:32:23 +0100

    fix rookie mistake

--- projects/examen/src/main/java/com/example/anisbenna/examen/controller/LocationController.java
+++ projects/examen/src/main/java/com/example/anisbenna/examen/controller/LocationController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.anisbenna.examen.models.Location;
 import com.example.anisbenna.examen.service.LocationService;
 
-@RestController("/location")
+@RestController
+@RequestMapping("/location")
 public class LocationController {
     @Autowired
     private LocationService locationService;
--- projects/examen/src/main/java/com/example/anisbenna/examen/controller/VehiculeController.java
+++ projects/examen/src/main/java/com/example/anisbenna/examen/controller/VehiculeController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.anisbenna.examen.models.Vehicule;
 import com.example.anisbenna.examen.service.VehiculeService;
 
-@RestController("/vehicule")
+@RestController
+@RequestMapping("/vehicule")
 public class VehiculeController {
     @Autowired
     public VehiculeService vehiculeService;
