77173db703310aefd4e2028565c2dc550abe35d6
usersina <benna.anis2@gmail.com>
2024-05-25 08:42:59 +0100

    initialize exam project

---
+++ projects/examen/src/main/java/com/example/anisbenna/examen/ExamenApplication.java
@@ -0,0 +1,11 @@
+package com.example.anisbenna.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenApplication.class, args);
+	}
+}
---
+++ projects/examen/src/test/java/com/example/anisbenna/examen/ExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anisbenna.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
